8231015ff07e4bb386b3dcddf21089b720a5df42
mtoohey31 <mtoohey31@users.noreply.github.com>
2021-09-07 14:36:03 -0400

    fix(web): avoid dangerous use of truthiness with predicted grade

--- web/src/components/GradeTable.svelte
+++ web/src/components/GradeTable.svelte
@@ -33,7 +33,7 @@
       rows={grade.grades.map((grade, i) => {
         grade.id = i;
         let predicted = grade.predicted();
-        grade.grade = predicted ? (predicted * 100).toFixed(2) + "%" : "--";
+        grade.grade = predicted !== null ? (predicted * 100).toFixed(2) + "%" : "--";
         return grade;
       })}
     >
